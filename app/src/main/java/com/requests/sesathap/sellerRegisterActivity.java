3c9598ac878cd150d54242ad85caef17386d57b0
Parami <lelkadaparami@gmail.com>
2021-03-19 16:22:17 +0530

    bg

---
+++ app/src/main/java/com/requests/sesathap/sellerRegisterActivity.java
@@ -0,0 +1,14 @@
+package com.requests.sesathap;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class sellerRegisterActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_seller_register);
+    }
+}
---
+++ app/src/main/java/com/requests/sesathap/userTypeSelActivity.java
@@ -0,0 +1,14 @@
+package com.requests.sesathap;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class userTypeSelActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_user_type_sel);
+    }
+}
