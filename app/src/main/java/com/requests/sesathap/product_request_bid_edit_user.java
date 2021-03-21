dd0c78562e6f805bfb8e424297eaf89d8eccc64b
RanganaPWM <pwmrangana@gmail.com>
2021-03-21 16:26:20 +0530

    Manoj MAD project

---
+++ app/src/main/java/com/requests/sesathap/product_request_bid_edit_user.java
@@ -0,0 +1,14 @@
+package com.requests.sesathap;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class product_request_bid_edit_user extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_product_request_bid_edit_user);
+    }
+}
---
+++ app/src/main/java/com/requests/sesathap/product_request_seller_orderview.java
@@ -0,0 +1,14 @@
+package com.requests.sesathap;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class product_request_seller_orderview extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_product_request_seller_orderview);
+    }
+}
