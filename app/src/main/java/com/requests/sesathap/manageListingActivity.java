a9dcafaa2725878c50205d7fcd045c90d4266cb9
Parami <lelkadaparami@gmail.com>
2021-03-22 06:58:37 +0530

    bg changes

---
+++ app/src/main/java/com/requests/sesathap/manageListingActivity.java
@@ -0,0 +1,14 @@
+package com.requests.sesathap;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class manageListingActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_manage_listing);
+    }
+}
