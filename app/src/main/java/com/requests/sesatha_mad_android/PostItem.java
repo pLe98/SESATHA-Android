92be482a2bf84c74b5bdf0f74f48a333e09fca71
SALUKA <salukadev4@gmail.com>
2021-05-04 19:54:29 +0530

    Save item data object to firestore

--- app/src/main/java/com/requests/sesatha_mad_android/PostItem.java
+++ app/src/main/java/com/requests/sesatha_mad_android/PostItem.java
@@ -6,6 +6,13 @@ import androidx.appcompat.widget.Toolbar;
 import androidx.drawerlayout.widget.DrawerLayout;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+
+import java.util.UUID;
 
 public class PostItem extends AppCompatActivity {
 
@@ -14,6 +21,12 @@ public class PostItem extends AppCompatActivity {
     DrawerLayout mdrawerLayout;
     ActionBarDrawerToggle mToggle;
     Toolbar mytoolbar;
+
+    //variables for firebase instances
+    DatabaseReference dbRef;
+
+    //Objects for elements
+    Button submiBtn;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -33,5 +46,21 @@ public class PostItem extends AppCompatActivity {
         mdrawerLayout.addDrawerListener(mToggle);
         mToggle.syncState();
         getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+
+        //get firebase instance
+        dbRef = FirebaseDatabase.getInstance("https://sesathaandroid-default-rtdb.asia-southeast1.firebasedatabase.app/").getReference();
+
+        //Set elements to objects
+        submiBtn = findViewById(R.id.btnPost);
+
+        submiBtn.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View v) {
+                    Item item = new Item("title1", "cat1", "desc1", 100.50f, "https", "dfsd3fdf1");
+                    dbRef.child("items").child(UUID.randomUUID().toString()).setValue(item);
+                }
+        }
+        );
     }
+
 }
