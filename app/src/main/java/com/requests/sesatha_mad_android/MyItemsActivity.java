11852ab5e92be83104320bbf1555edd0e4713d86
SALUKA <salukadev4@gmail.com>
2021-05-07 04:08:41 +0530

    create MyItemsActivity

---
+++ app/src/main/java/com/requests/sesatha_mad_android/MyItemsActivity.java
@@ -0,0 +1,14 @@
+package com.requests.sesatha_mad_android;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MyItemsActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_my_items);
+    }
+}
