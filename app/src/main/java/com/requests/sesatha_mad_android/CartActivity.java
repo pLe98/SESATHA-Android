9a459e0045962baec3e9a38ee85cce7544954741
Parami <lelkadaparami@gmail.com>
2021-05-08 04:43:13 +0530

    cart function

--- app/src/main/java/com/requests/sesatha_mad_android/CartActivity.java
+++ app/src/main/java/com/requests/sesatha_mad_android/CartActivity.java
@@ -1,19 +1,24 @@
 package com.requests.sesatha_mad_android;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
 import android.os.Bundle;
 
+
+import com.firebase.ui.database.FirebaseRecyclerOptions;
 import com.google.firebase.database.FirebaseDatabase;
 import com.google.firebase.database.Query;
-import com.requests.sesatha_mad_android.adapters.myItemsAdapter;
+import com.requests.sesatha_mad_android.adapters.cartItemAdapter;
+import com.requests.sesatha_mad_android.models.Cart;
+
 
 public class CartActivity extends AppCompatActivity {
 
-    private RecyclerView cartItem;
-    myItemsAdapter adapter;
-    Query data;
+    private RecyclerView recyViewCart;
+    cartItemAdapter adapter;
+    Query db;
 
     private String userID;
     GlobalClass globalVariables;
@@ -26,6 +31,26 @@ public class CartActivity extends AppCompatActivity {
         globalVariables = (GlobalClass) getApplicationContext();
         userID = globalVariables.getUser().getUserID();
 
-        data = FirebaseDatabase.getInstance("https://sesathaandroid-default-rtdb.asia-southeast1.firebasedatabase.app/").getReference().child("Cart").child(userID).limitToFirst(10);
+        db = FirebaseDatabase.getInstance("https://sesathaandroid-default-rtdb.asia-southeast1.firebasedatabase.app/").getReference().child("Cart").child(userID);
+
+        recyViewCart = findViewById(R.id.cart_recycler);
+        recyViewCart.setLayoutManager(new LinearLayoutManager(this));
+
+        FirebaseRecyclerOptions<Cart> data= new FirebaseRecyclerOptions.Builder<Cart>().setQuery(db,Cart.class).build();
+
+        adapter = new cartItemAdapter(data);
+        recyViewCart.setAdapter(adapter);
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+        adapter.startListening(); //adapter keeps updating the recycler view when realtime-db updates
+    }
+
+    @Override
+    protected void onStop() {
+        super.onStop();
+        adapter.stopListening(); //Stops getting new data when activity exits
     }
 }
--- app/src/main/java/com/requests/sesatha_mad_android/adapters/cartItemAdapter.java
+++ app/src/main/java/com/requests/sesatha_mad_android/adapters/cartItemAdapter.java
@@ -1,34 +1,59 @@
 package com.requests.sesatha_mad_android.adapters;
 
+import android.view.LayoutInflater;
+import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.TextView;
 
 import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
 
 import com.firebase.ui.database.FirebaseRecyclerAdapter;
 import com.firebase.ui.database.FirebaseRecyclerOptions;
-import com.requests.sesatha_mad_android.models.Item;
+import com.requests.sesatha_mad_android.R;
+import com.requests.sesatha_mad_android.models.Cart;
 
-public class cartItemAdapter extends FirebaseRecyclerAdapter<Item, myItemsAdapter.itemsViewHolder> {
+
+public class cartItemAdapter extends FirebaseRecyclerAdapter<Cart, cartItemAdapter.cartItemsViewHolder> {
     /**
      * Initialize a {@link RecyclerView.Adapter} that listens to a Firebase query. See
      * {@link FirebaseRecyclerOptions} for configuration options.
      *
      * @param options
      */
-    public cartItemAdapter(@NonNull FirebaseRecyclerOptions<Item> options) {
+    public cartItemAdapter(@NonNull FirebaseRecyclerOptions<Cart> options) {
         super(options);
     }
 
     @Override
-    protected void onBindViewHolder(@NonNull myItemsAdapter.itemsViewHolder holder, int position, @NonNull Item model) {
-
-
-
+    protected void onBindViewHolder(@NonNull cartItemAdapter.cartItemsViewHolder holder, int position, @NonNull Cart model) {
+        holder.title.setText(model.getTitle());
+        holder.unitPrice.setText(String.valueOf(model.getUnitPrice()));
+        holder.shipping.setText(String.valueOf(model.getShipping()));
+        holder.qty.setText(String.valueOf(model.getQty()));
+        holder.itemNo.setText(model.getItemNo());
     }
 
     @NonNull
     @Override
-    public myItemsAdapter.itemsViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
-        return null;
+    public cartItemAdapter.cartItemsViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        View view = LayoutInflater.from(parent.getContext()).inflate(R.layout.cart_item_card,parent,false);
+        return new cartItemAdapter.cartItemsViewHolder(view);
+    }
+
+    public class cartItemsViewHolder extends RecyclerView.ViewHolder {
+        //Float unitPrice;
+        TextView itemNo, title,unitPrice, qty, shipping;
+        public cartItemsViewHolder(@NonNull View itemView) {
+            super(itemView);
+
+            title = itemView.findViewById(R.id.citem_title);
+            itemNo = itemView.findViewById(R.id.citem_itemno);
+            unitPrice = itemView.findViewById(R.id.citem_unitprice);
+            qty = itemView.findViewById(R.id.citem_qty);
+            shipping = itemView.findViewById(R.id.citem_shipping);
+
+        }
     }
 }
