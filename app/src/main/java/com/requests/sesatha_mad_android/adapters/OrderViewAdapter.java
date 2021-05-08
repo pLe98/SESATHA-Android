3d1d65bb4a5ba07e18aa9d4aed0d3e9f2672c81f
ridma11 <ridmika.reshan11@gmail.com>
2021-05-08 22:14:57 +0530

    Order View Function created

---
+++ app/src/main/java/com/requests/sesatha_mad_android/adapters/OrderViewAdapter.java
@@ -0,0 +1,58 @@
+package com.requests.sesatha_mad_android.adapters;
+
+import android.content.Context;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
+
+import com.requests.sesatha_mad_android.R;
+import com.requests.sesatha_mad_android.models.OrderDetails;
+import java.util.ArrayList;
+public class OrderViewAdapter extends RecyclerView.Adapter<OrderViewAdapter.MyViewHolder> {
+
+    Context context;
+    ArrayList <OrderDetails> arrayList;
+
+    public OrderViewAdapter(Context context, ArrayList<OrderDetails> arrayList) {
+        this.context = context;
+        this.arrayList = arrayList;
+    }
+
+    @NonNull
+    @Override
+    public MyViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        View v = LayoutInflater.from(context).inflate(R.layout.activity_order_view,parent,false);
+        return new MyViewHolder(v);
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull MyViewHolder holder, int position) {
+        OrderDetails OrderDetails = arrayList.get(position);
+        holder.productname.setText(OrderDetails.getProductName());
+        holder.quantitiy.setText(OrderDetails.getProductName());
+        holder.total.setText(OrderDetails.getAmount());
+
+    }
+
+    @Override
+    public int getItemCount() {
+
+        return arrayList.size();
+    }
+
+    public static class MyViewHolder extends  RecyclerView.ViewHolder {
+
+        TextView productname, quantitiy , total;
+        public MyViewHolder(@NonNull View itemView) {
+            super(itemView);
+
+            productname =itemView.findViewById(R.id.product);
+            quantitiy= itemView.findViewById(R.id.quantity);
+            total =itemView.findViewById(R.id.totPrice);
+        }
+    }
+}
