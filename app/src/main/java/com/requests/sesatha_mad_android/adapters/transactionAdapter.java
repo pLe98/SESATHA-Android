642e3aa9c5cf87e3390874df261802adc0ae9a69
Parami <lelkadaparami@gmail.com>
2021-05-08 22:54:31 +0530

    transaction histry, clear cart

--- app/src/main/java/com/requests/sesatha_mad_android/adapters/transactionAdapter.java
+++ app/src/main/java/com/requests/sesatha_mad_android/adapters/transactionAdapter.java
@@ -1,4 +1,54 @@
 package com.requests.sesatha_mad_android.adapters;
 
-public class transactionAdapter {
+import android.util.Log;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
+
+import com.firebase.ui.database.FirebaseRecyclerAdapter;
+import com.firebase.ui.database.FirebaseRecyclerOptions;
+import com.requests.sesatha_mad_android.R;
+import com.requests.sesatha_mad_android.models.Transaction;
+
+public class transactionAdapter extends FirebaseRecyclerAdapter<Transaction, transactionAdapter.transactionViewHolder> {
+    /**
+     * Initialize a {@link RecyclerView.Adapter} that listens to a Firebase query. See
+     * {@link FirebaseRecyclerOptions} for configuration options.
+     *
+     * @param options
+     */
+    public transactionAdapter(@NonNull FirebaseRecyclerOptions<Transaction> options) {
+        super(options);
+
+    }
+
+
+    @Override
+    protected void onBindViewHolder(@NonNull transactionViewHolder holder, int position, @NonNull Transaction model) {
+        Log.d("Trans",model.getOrderDate());
+        holder.transID.setText(model.getTransactionID());
+        holder.amount.setText(String.valueOf(model.getAmount()));
+        holder.transDate.setText(model.getOrderDate());
+    }
+
+    @NonNull
+    @Override
+    public transactionViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        View view = LayoutInflater.from(parent.getContext()).inflate(R.layout.transaction_cards,parent,false);
+        return new transactionAdapter.transactionViewHolder(view);
+    }
+
+    public class transactionViewHolder extends RecyclerView.ViewHolder {
+        TextView transID, amount, transDate;
+        public transactionViewHolder(@NonNull View itemView) {
+            super(itemView);
+            transDate = itemView.findViewById(R.id.trans_date);
+            transID = itemView.findViewById(R.id.trans_transid);
+            amount = itemView.findViewById(R.id.trans_amount);
+        }
+    }
 }
--- app/src/main/java/com/requests/sesatha_mad_android/models/Transaction.java
+++ app/src/main/java/com/requests/sesatha_mad_android/models/Transaction.java
@@ -1,4 +1,40 @@
 package com.requests.sesatha_mad_android.models;
 
 public class Transaction {
+
+    private String transactionID, orderDate;
+    private float amount;
+
+    public Transaction() {
+    }
+
+    public Transaction(String transactionID, String orderDate, float amount) {
+        this.transactionID = transactionID;
+        this.orderDate = orderDate;
+        this.amount = amount;
+    }
+
+    public String getTransactionID() {
+        return transactionID;
+    }
+
+    public void setTransactionID(String transactionID) {
+        this.transactionID = transactionID;
+    }
+
+    public String getOrderDate() {
+        return orderDate;
+    }
+
+    public void setOrderDate(String orderDate) {
+        this.orderDate = orderDate;
+    }
+
+    public float getAmount() {
+        return amount;
+    }
+
+    public void setAmount(float amount) {
+        this.amount = amount;
+    }
 }
