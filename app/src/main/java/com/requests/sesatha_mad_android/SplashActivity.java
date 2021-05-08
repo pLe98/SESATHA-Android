09fc23a97a1e8737e9302132208f4762999416b0
SALUKA <salukadev4@gmail.com>
2021-05-09 00:24:45 +0530

    Splash screen

---
+++ app/src/main/java/com/requests/sesatha_mad_android/SplashActivity.java
@@ -0,0 +1,35 @@
+package com.requests.sesatha_mad_android;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.Handler;
+
+public class SplashActivity extends AppCompatActivity {
+    private Handler WaitHandler = new Handler();
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash);
+
+        WaitHandler.postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                //Go to next main activity.
+                Intent intent = new Intent(getApplicationContext(), MainActivity.class);
+                startActivity(intent);
+
+                finish();
+            }
+        },1600);
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        WaitHandler.removeCallbacksAndMessages(null);
+    }
+}
