32080fe22c15019e892ea2647578c34334677df2
MANOJ <pwmrangana@gmail.com>
2021-05-09 00:34:28 +0530

    plus function

--- app/src/main/java/com/requests/sesatha_mad_android/ItemDescriptionActivity.java
+++ app/src/main/java/com/requests/sesatha_mad_android/ItemDescriptionActivity.java
@@ -88,6 +88,7 @@ public class ItemDescriptionActivity extends AppCompatActivity {
             }
         });
 
+        //plus function
         plus.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
