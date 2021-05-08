16da5e58ce9045d2b0fa7ad1a9170c6e85358182
ridma11 <ridmika.reshan11@gmail.com>
2021-05-08 22:17:31 +0530

    Order View Function created

---
+++ app/src/main/java/com/requests/sesatha_mad_android/models/OrderDetails.java
@@ -0,0 +1,31 @@
+package com.requests.sesatha_mad_android.models;
+
+public class OrderDetails {
+
+    String productName;
+    int quantity;
+    int amount;
+
+
+    private OrderDetails(){}
+
+    private OrderDetails(String productName, int quantity, int amount){
+        this.productName=productName;
+        this.quantity=quantity;
+        this.amount=amount;
+    }
+
+    public String getProductName() {
+        return productName;
+    }
+
+    public int getQuantity() {
+        return quantity;
+    }
+
+    public int getAmount() {
+        return amount;
+    }
+
+
+}
