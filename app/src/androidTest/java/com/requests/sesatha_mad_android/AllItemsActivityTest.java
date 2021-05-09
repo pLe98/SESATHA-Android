8fa5dde33517f27923953c593cea1b5f4377a038
SALUKA <salukadev4@gmail.com>
2021-05-09 19:17:54 +0530

    Instrumentation tests

---
+++ app/src/androidTest/java/com/requests/sesatha_mad_android/AllItemsActivityTest.java
@@ -0,0 +1,33 @@
+package com.requests.sesatha_mad_android;
+
+import android.content.Intent;
+import android.view.View;
+
+import androidx.test.rule.ActivityTestRule;
+
+import org.junit.Before;
+import org.junit.Rule;
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class AllItemsActivityTest {
+
+    @Rule
+    public ActivityTestRule<AllItemsActivity> activityRule
+            = new ActivityTestRule<AllItemsActivity>(
+            AllItemsActivity.class);
+    private AllItemsActivity testActivity = null;
+
+    @Before
+    public void setUp() throws Exception{
+        testActivity =  activityRule.getActivity();
+    }
+
+
+    @Test
+    public void intent() {
+        View view = testActivity.findViewById(R.id.AllListingsRecycler);
+        assertNotNull(view);
+    }
+}
